58400cf461252b30ca323c5e28906e2853fa290e
Kevin Hrpcek <khrpcek@ucar.edu>
2026-02-12 15:58:53 -0700

    stormspeed

--- containers/stormspeed/mpi_setup.sh
+++ containers/stormspeed/mpi_setup.sh
@@ -1,13 +1,12 @@
 git config --global user.email "example_user@example.com"
 git config --global user.name "example_user"
 cd /stormspeed/cime/scripts
-./create_newcase --case /tmp/ci_test/one --machine cirrus --compset FADIAB --res ne30_ne30_mg17 --compiler intel  --run-unsupported
+./create_newcase --case /tmp/ci_test --machine cirrus --compset FADIAB --res ne30_ne30_mg17 --compiler intel  --run-unsupported
 
 echo "StrictHostKeyChecking no" >> /etc/ssh/ssh_config
 echo "UserKnownHostsFile /dev/null" >> /etc/ssh/ssh_config
 
-
-cd /tmp/ci_test/one
+cd /tmp/ci_test
 ./xmlchange NTASKS=20
 ./xmlchange CAM_TARGET='theta0-1'
 ./xmlchange STOP_OPTION=ndays,STOP_N=1,RESUBMIT=0
@@ -28,7 +27,7 @@ cd /tmp/ci_test/one
 ########################
 # Output daily average #
 ########################
-cd /tmp/ci_test/one
+cd /tmp/ci_test
 cat <<EOF > user_nl_cam
 se_statefreq = 488,
 mfilt=1,
@@ -45,6 +44,8 @@ EOF
 ################
 # Run the test #
 ################
+. /opt/spack/share/spack/setup-env.sh
+spack load gcc prrte
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PIO/lib:$NETCDF_FORTRAN_PATH/lib:$NETCDF_C_PATH/lib:$LAPACK/lib:$LAPACK/lib64:$PNETCDF/lib
 #if [ "${{ matrix.runner }}" == "gha-runner-gpu-stormspeed" ]; then
 #export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CUDA_ROOT/lib64:$CUDA_ROOT/lib64/stubs
