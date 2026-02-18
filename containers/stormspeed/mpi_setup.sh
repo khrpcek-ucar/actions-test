bb42e8b5afe5cbb562fc5862b5074aad415cabf0
Kevin Hrpcek <khrpcek@ucar.edu>
2026-02-17 17:16:03 -0700

    stormspeed

--- containers/stormspeed/mpi_setup.sh
+++ containers/stormspeed/mpi_setup.sh
@@ -6,7 +6,7 @@ cd /stormspeed/cime/scripts
 echo "StrictHostKeyChecking no" >> /etc/ssh/ssh_config
 echo "UserKnownHostsFile /dev/null" >> /etc/ssh/ssh_config
 
-cd /mpi/ci_test
+cd /tmp/ci_test
 ./xmlchange NTASKS=20
 ./xmlchange CAM_TARGET='theta0-1'
 ./xmlchange STOP_OPTION=ndays,STOP_N=1,RESUBMIT=0
@@ -27,7 +27,7 @@ cd /mpi/ci_test
 ########################
 # Output daily average #
 ########################
-cd /mpi/ci_test
+cd /tmp/ci_test
 cat <<EOF > user_nl_cam
 se_statefreq = 488,
 mfilt=1,
